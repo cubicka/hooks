907a063a973caec0eb56d5cdb6e76968e0225840
Ahmad Agung A <ahmad.agung.a@gmail.com>
2020-03-11 13:20:14 +0700

    Move onChange of useDeviceOrientation into useEffect and fix bug with its state

--- src/useDeviceOrientation.ts
+++ src/useDeviceOrientation.ts
@@ -1,43 +1,32 @@
-import {useEffect, useState, useCallback} from 'react'
-import {Dimensions, ScaledSize} from 'react-native'
+import { useCallback, useEffect, useState } from 'react'
+import { Dimensions, ScaledSize } from 'react-native'
 
-const screen = Dimensions.get('screen')
+const isOrientationLandscape = ({ width, height }: { width: number; height: number }) => width >= height
+const isOrientationPortrait = ({ width, height }: { width: number; height: number }) => height >= width
 
 export default function() {
-  const isOrientationPortrait = ({
-    width,
-    height,
-  }: {
-    width: number
-    height: number
-  }) => height >= width
-  const isOrientationLandscape = ({
-    width,
-    height,
-  }: {
-    width: number
-    height: number
-  }) => width >= height
-
-  const [orientation, setOrientation] = useState({
-    portrait: isOrientationPortrait(screen),
-    landscape: isOrientationLandscape(screen),
-  })
-
-  const onChange = useCallback(({screen}: {screen: ScaledSize}) => {
-    setOrientation({
+  const [orientation, setOrientation] = useState(() => {
+    const screen = Dimensions.get('screen')
+    return {
       portrait: isOrientationPortrait(screen),
       landscape: isOrientationLandscape(screen),
-    })
-  }, [])
+    }
+  })
 
   useEffect(() => {
+    const onChange = useCallback(({ screen }: { screen: ScaledSize }) => {
+      setOrientation({
+        portrait: isOrientationPortrait(screen),
+        landscape: isOrientationLandscape(screen),
+      })
+    }, [])
+
     Dimensions.addEventListener('change', onChange)
 
     return () => {
       Dimensions.removeEventListener('change', onChange)
     }
-  }, [orientation.portrait, orientation.landscape, onChange])
+  }, [])
 
   return orientation
 }
